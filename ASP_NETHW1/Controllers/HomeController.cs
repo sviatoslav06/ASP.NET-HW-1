fa2a16c7d494f376b768bf7f2dedb36225a0fc84
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2023-09-16 16:19:07 +0300

    Add project files.

---
+++ ASP_NETHW1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ASP.NET_HW_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_HW_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Films()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
